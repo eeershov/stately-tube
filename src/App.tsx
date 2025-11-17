38bc69248169c0458dd92c9d6437adc0a9a3bfaa
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2025-11-17 19:51:21 +0300

    init: vite

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <video
+        width={1000}
+        src="https://cdn.flowplayer.com/d9cd469f-14fc-4b7b-a7f6-ccbfa755dcb8/hls/383f752a-cbd1-4691-a73f-a4e583391b3d/playlist.m3u8"
+      >
+        vid
+      </video>
+    </>
+  );
+}
+
+export default App;
