15a3d542eaa13e63bcc6ac693e40a09daf07216c
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2025-11-18 23:38:09 +0300

    feat: added one more video

--- src/consts.ts
+++ src/consts.ts
@@ -11,4 +11,8 @@ export const VIDEOS = [
     title: "Plaid shirt happiness",
     url: "https://cdn.flowplayer.com/d9cd469f-14fc-4b7b-a7f6-ccbfa755dcb8/hls/383f752a-cbd1-4691-a73f-a4e583391b3d/playlist.m3u8",
   },
+  {
+    title: "The Cinematic Orchestra",
+    url: "https://www.youtube.com/watch?v=oUFJJNQGwhk",
+  },
 ];
